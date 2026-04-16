583ba602f0c66162c5e83a3c9b91b9aadc1cece1
Salah-Othman <Salahothmansaad@gmail.com>
2026-04-17 01:47:16 +0200

    Refactor main app structure and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food/core/routing/app_router.dart';
+import 'package:food/core/routing/routes.dart';
+import 'package:food/core/theme/app_theme.dart';
 import 'package:food/features/home/ui/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(FoodApp(appRouter: AppRouter()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FoodApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const FoodApp({super.key, required this.appRouter});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen());
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      builder: (context, child) {
+        return MaterialApp(
+          theme: AppTheme.lightTheme,
+          debugShowCheckedModeBanner: false,
+          home: HomeScreen(),
+          initialRoute: Routes.onBoardingScreen,
+          onGenerateRoute: appRouter.generateRoute,
+        );
+      },
+    );
   }
 }

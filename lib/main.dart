eb60ca4af2f66686d695ee0f644c97c33d24d7d9
Salah-Othman <Salahothmansaad@gmail.com>
2026-04-17 13:14:34 +0200

    Refactor main

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:food/core/routing/app_router.dart';
 import 'package:food/core/routing/routes.dart';
 import 'package:food/core/theme/app_theme.dart';
-import 'package:food/features/home/ui/home_screen.dart';
+import 'package:food/features/onboarding/ui/onboarding_screen.dart';
 
 void main() {
   runApp(FoodApp(appRouter: AppRouter()));
@@ -14,7 +14,7 @@ class FoodApp extends StatelessWidget {
   const FoodApp({super.key, required this.appRouter});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {   
     return ScreenUtilInit(
       designSize: const Size(375, 812),
       minTextAdapt: true,
@@ -22,7 +22,7 @@ class FoodApp extends StatelessWidget {
         return MaterialApp(
           theme: AppTheme.lightTheme,
           debugShowCheckedModeBanner: false,
-          home: HomeScreen(),
+          home: OnBoardingScreen(),
           initialRoute: Routes.onBoardingScreen,
           onGenerateRoute: appRouter.generateRoute,
         );
